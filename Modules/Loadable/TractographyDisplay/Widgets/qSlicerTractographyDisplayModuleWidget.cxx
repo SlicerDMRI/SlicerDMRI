SlicerDMRI/SlicerDMRI

commit e5db1714d8137694292f7eabb5f2a34b03140d70

ENH: Interface improvement in the tractography display module: consistent selection of the nodes
git-svn-id: http://svn.slicer.org/Slicer4/trunk@20538 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/TractographyDisplay/Widgets/qSlicerTractographyDisplayModuleWidget.cxx
@@ -23,6 +23,7 @@
 #include "qMRMLSceneTractographyDisplayModel.h"
 // MRML includes
 
+#include "vtkMRMLNode.h"
 #include "vtkMRMLFiberBundleNode.h"
 #include "vtkMRMLFiberBundleDisplayNode.h"
 #include "vtkMRMLFiberBundleStorageNode.h"
@@ -71,7 +72,6 @@ void qSlicerTractographyDisplayModuleWidgetPrivate::init()
   QObject::connect(this->TractographyDisplayTreeView, SIGNAL(visibilityChanged(int)),
                    this->tabWidget, SLOT(setCurrentIndex (int)));
 
-  
 }
 
 
@@ -104,22 +104,25 @@ void qSlicerTractographyDisplayModuleWidget::setFiberBundleNode(vtkMRMLNode* inp
 void qSlicerTractographyDisplayModuleWidget::setFiberBundleNode(vtkMRMLFiberBundleNode* fiberBundleNode)
 {
   Q_D(qSlicerTractographyDisplayModuleWidget);
-  if (vtkMRMLFiberBundleNode::SafeDownCast(fiberBundleNode))
-    {
-    d->fiberBundleNode = fiberBundleNode;
 
-    d->LineDisplayWidget->setFiberBundleNode(fiberBundleNode);
-    d->TubeDisplayWidget->setFiberBundleNode(fiberBundleNode);
-    d->GlyphDisplayWidget->setFiberBundleNode(fiberBundleNode);
+  if (d->fiberBundleNode == fiberBundleNode)
+    return;
 
-    d->LineDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetLineDisplayNode());
-    d->TubeDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetTubeDisplayNode());
-    d->GlyphDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
-    d->GlyphPropertiesWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
-    
-    d->PercentageOfFibersShown = fiberBundleNode->GetSubsamplingRatio() * 100.;
-    emit percentageOfFibersShownChanged(d->PercentageOfFibersShown);
-    }
+  d->fiberBundleNode = fiberBundleNode;
+
+  d->LineDisplayWidget->setFiberBundleNode(fiberBundleNode);
+  d->TubeDisplayWidget->setFiberBundleNode(fiberBundleNode);
+  d->GlyphDisplayWidget->setFiberBundleNode(fiberBundleNode);
+
+  d->LineDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetLineDisplayNode());
+  d->TubeDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetTubeDisplayNode());
+  d->GlyphDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
+  d->GlyphPropertiesWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
+  
+  d->PercentageOfFibersShown = fiberBundleNode->GetSubsamplingRatio() * 100.;
+
+  emit currentNodeChanged(d->fiberBundleNode);
+  emit percentageOfFibersShownChanged(d->PercentageOfFibersShown);
 }
 
 void qSlicerTractographyDisplayModuleWidget::setPercentageOfFibersShown(double percentage)

Modules/Loadable/TractographyDisplay/Widgets/Resources/UI/qSlicerTractographyDisplayModule.ui
@@ -62,29 +62,6 @@
     </widget>
    </item>
    <item>
-    <layout class="QHBoxLayout" name="horizontalLayout">
-     <item>
-      <widget class="QLabel" name="SelectFiberBundleLabel">
-       <property name="text">
-        <string>FiberBundle:</string>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="qMRMLNodeComboBox" name="FiberBundleNodeSelector">
-       <property name="enabled">
-        <bool>true</bool>
-       </property>
-       <property name="nodeTypes">
-        <stringlist>
-         <string>vtkMRMLFiberBundleNode</string>
-        </stringlist>
-       </property>
-      </widget>
-     </item>
-    </layout>
-   </item>
-   <item>
     <widget class="QWidget" name="widget_3" native="true">
      <layout class="QFormLayout" name="formLayout">
       <property name="fieldGrowthPolicy">
@@ -167,7 +144,7 @@
          </sizepolicy>
         </property>
         <property name="currentIndex">
-         <number>0</number>
+         <number>2</number>
         </property>
         <widget class="QWidget" name="tab">
          <attribute name="title">
@@ -303,11 +280,6 @@
  </widget>
  <customwidgets>
   <customwidget>
-   <class>qMRMLNodeComboBox</class>
-   <extends>QWidget</extends>
-   <header>qMRMLNodeComboBox.h</header>
-  </customwidget>
-  <customwidget>
    <class>qSlicerWidget</class>
    <extends>QWidget</extends>
    <header>qSlicerWidget.h</header>
@@ -351,42 +323,14 @@
    <class>qMRMLTractographyDisplayTreeView</class>
    <extends>QTreeView</extends>
    <header>qMRMLTractographyDisplayTreeView.h</header>
+   <slots>
+    <signal>currentNodeChanged(vtkMRMLNode*)</signal>
+    <slot>setCurrentNode(vtkMRMLNode*)</slot>
+   </slots>
   </customwidget>
  </customwidgets>
  <resources/>
  <connections>
-  <connection>
-   <sender>qSlicerTractographyDisplayModule</sender>
-   <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
-   <receiver>FiberBundleNodeSelector</receiver>
-   <slot>setMRMLScene(vtkMRMLScene*)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>161</x>
-     <y>115</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>205</x>
-     <y>22</y>
-    </hint>
-   </hints>
-  </connection>
-  <connection>
-   <sender>FiberBundleNodeSelector</sender>
-   <signal>currentNodeChanged(vtkMRMLNode*)</signal>
-   <receiver>qSlicerTractographyDisplayModule</receiver>
-   <slot>setFiberBundleNode(vtkMRMLNode*)</slot>
-   <hints>
-    <hint type="sourcelabel">
-     <x>205</x>
-     <y>22</y>
-    </hint>
-    <hint type="destinationlabel">
-     <x>161</x>
-     <y>115</y>
-    </hint>
-   </hints>
-  </connection>
   <connection>
    <sender>qSlicerTractographyDisplayModule</sender>
    <signal>mrmlSceneChanged(vtkMRMLScene*)</signal>
@@ -398,8 +342,8 @@
      <y>115</y>
     </hint>
     <hint type="destinationlabel">
-     <x>161</x>
-     <y>87</y>
+     <x>135</x>
+     <y>443</y>
     </hint>
    </hints>
   </connection>
@@ -414,8 +358,8 @@
      <y>115</y>
     </hint>
     <hint type="destinationlabel">
-     <x>161</x>
-     <y>87</y>
+     <x>111</x>
+     <y>443</y>
     </hint>
    </hints>
   </connection>
@@ -430,8 +374,8 @@
      <y>43</y>
     </hint>
     <hint type="destinationlabel">
-     <x>222</x>
-     <y>121</y>
+     <x>111</x>
+     <y>443</y>
     </hint>
    </hints>
   </connection>
@@ -446,8 +390,8 @@
      <y>115</y>
     </hint>
     <hint type="destinationlabel">
-     <x>161</x>
-     <y>103</y>
+     <x>135</x>
+     <y>463</y>
     </hint>
    </hints>
   </connection>
@@ -462,8 +406,8 @@
      <y>349</y>
     </hint>
     <hint type="destinationlabel">
-     <x>174</x>
-     <y>48</y>
+     <x>198</x>
+     <y>127</y>
     </hint>
    </hints>
   </connection>
@@ -478,26 +422,29 @@
      <y>349</y>
     </hint>
     <hint type="destinationlabel">
-     <x>210</x>
-     <y>143</y>
+     <x>234</x>
+     <y>351</y>
     </hint>
    </hints>
   </connection>
   <connection>
-   <sender>FiberBundleNodeSelector</sender>
+   <sender>TractographyDisplayTreeView</sender>
    <signal>currentNodeChanged(vtkMRMLNode*)</signal>
-   <receiver>ROIEditorWidget</receiver>
+   <receiver>qSlicerTractographyDisplayModule</receiver>
    <slot>setFiberBundleNode(vtkMRMLNode*)</slot>
    <hints>
     <hint type="sourcelabel">
-     <x>254</x>
-     <y>23</y>
+     <x>277</x>
+     <y>184</y>
     </hint>
     <hint type="destinationlabel">
-     <x>210</x>
-     <y>143</y>
+     <x>415</x>
+     <y>15</y>
     </hint>
    </hints>
   </connection>
  </connections>
+ <slots>
+  <slot>setFiberBundleNode(vtkMRMLNode*)</slot>
+ </slots>
 </ui>

Modules/Loadable/TractographyDisplay/Widgets/qMRMLTractographyDisplayTreeView.cxx
@@ -98,8 +98,8 @@ void qMRMLTractographyDisplayTreeViewPrivate::init()
   //ctkModelTester * tester = new ctkModelTester(p);
   //tester->setModel(this->SortFilterModel);
 
-  QObject::connect(q, SIGNAL(clicked(QModelIndex)),
-                   q, SLOT(onClicked(QModelIndex)));
+  //QObject::connect(q, SIGNAL(clicked(QModelIndex)),
+  //                 q, SLOT(onClicked(QModelIndex)));
 
   /***
   QObject::connect( q->selectionModel(),

Modules/Loadable/TractographyDisplay/Widgets/qMRMLTractographyDisplayTreeView.h
@@ -62,10 +62,10 @@ public:
 
 
 signals:
-  void currentNodeChanged(vtkMRMLNode* node);
   void onPropertyEditButtonClicked(QString id);
   // type 0-line 1-tube 2-glyph
   void visibilityChanged(int type);
+public slots:
 
 
 protected:

Modules/Loadable/TractographyDisplay/Widgets/qSlicerTractographyDisplayModuleWidget.h
@@ -51,6 +51,8 @@ public slots:
 
 signals:
   void percentageOfFibersShownChanged(double);
+  void currentNodeChanged(vtkMRMLNode*);
+  void currentNodeChanged(vtkMRMLFiberBundleNode*);
 
 protected:
   virtual void setup();

commit 6c98e60e3202c77b739dcfca9cb9fca4f699db35

BUG: Fixed possible crash introduced in r20538
git-svn-id: http://svn.slicer.org/Slicer4/trunk@20617 3bd1e089-480b-0410-8dfb-8563597acbee

Modules/Loadable/TractographyDisplay/Widgets/qSlicerTractographyDisplayModuleWidget.cxx
@@ -110,6 +110,7 @@ void qSlicerTractographyDisplayModuleWidget::setFiberBundleNode(vtkMRMLFiberBund
 
   d->fiberBundleNode = fiberBundleNode;
 
+
   d->LineDisplayWidget->setFiberBundleNode(fiberBundleNode);
   d->TubeDisplayWidget->setFiberBundleNode(fiberBundleNode);
   d->GlyphDisplayWidget->setFiberBundleNode(fiberBundleNode);
@@ -120,9 +121,9 @@ void qSlicerTractographyDisplayModuleWidget::setFiberBundleNode(vtkMRMLFiberBund
     d->TubeDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetTubeDisplayNode());
     d->GlyphDisplayWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
     d->GlyphPropertiesWidget->setFiberBundleDisplayNode(fiberBundleNode->GetGlyphDisplayNode());
-    
     d->PercentageOfFibersShown = fiberBundleNode->GetSubsamplingRatio() * 100.;
   }
+
   emit currentNodeChanged(d->fiberBundleNode);
   emit percentageOfFibersShownChanged(d->PercentageOfFibersShown);
 }