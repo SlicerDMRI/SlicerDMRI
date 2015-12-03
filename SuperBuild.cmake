0402ca481affc4a881c1bd0132c6daa6f7643f00
Isaiah Norton <inorton@bwh.harvard.edu>
2015-12-03 17:01:25 -0500

    COMP: Add merged modules to SuperBuild COMP: Fix TractographyDisplay in superbuild

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -27,7 +27,7 @@ foreach(dep ${EXTENSION_DEPENDS})
 endforeach()
 
 set(proj ${SUPERBUILD_TOPLEVEL_PROJECT})
-set(${proj}_DEPENDS Foo)
+set(${proj}_DEPENDS "")
 
 ExternalProject_Include_Dependencies(${proj}
   PROJECT_VAR proj
