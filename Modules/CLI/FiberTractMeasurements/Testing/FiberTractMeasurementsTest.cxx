a82fde4870cbf03d8e418b01464cc23d5891d98f
Isaiah Norton <isaiah.norton@gmail.com>
2016-02-25 16:01:11 -0500

    COMP: fix windows build error due to outdated CLI entrypoint

--- Modules/CLI/FiberTractMeasurements/Testing/FiberTractMeasurementsTest.cxx
+++ Modules/CLI/FiberTractMeasurements/Testing/FiberTractMeasurementsTest.cxx
@@ -1,15 +1,11 @@
 #include <iostream>
 #include "itkTestMain.h"
 
-#ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
-#else
-#define MODULE_IMPORT
-#endif
-
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []);
-
 void RegisterTests()
 {
-  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+  REGISTER_TEST(FiberTractMeasurementsTest);
 }
+
+#undef main
+#define main FiberTractMeasurementsTest
+#include "../FiberTractMeasurements.cxx"
