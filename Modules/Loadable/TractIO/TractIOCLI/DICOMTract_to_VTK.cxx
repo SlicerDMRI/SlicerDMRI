d610ab8b03f4a47d37e34da8d2b63c2b38b10c45
Isaiah Norton <isaiah.norton@gmail.com>
2016-09-13 13:31:07 -0400

    ENH: add initial DICOM tractio features

---
+++ Modules/Loadable/TractIO/TractIOCLI/DICOMTract_to_VTK.cxx
@@ -0,0 +1,185 @@
+/*=========================================================================
+
+  Program:   Realign Volumes
+  Module:    $HeadURL$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See License.txt or http://www.slicer.org/copyright/copyright.txt for details.
+
+==========================================================================*/
+#include "vtkPluginFilterWatcher.h"
+
+// MRML includes
+#include <vtkMRMLLinearTransformNode.h>
+#include <vtkMRMLScene.h>
+
+// VTK includes
+#include <vtkMath.h>
+#include <vtkNew.h>
+#include <vtkPolyData.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+
+// DCMTK includes
+#include "dcmtk/dcmtract/trctrack.h"
+#include "dcmtk/dcmtract/trctrackset.h"
+#include "dcmtk/dcmtract/trctractographyresults.h"
+
+#ifndef NDEBUG
+  #define DBGOUT std::cout
+#else
+  #define DBGOUT if(false) std::cout
+#endif
+
+// SlicerSEM includes
+#ifdef HAVE_SSTREAM
+#undef HAVE_SSTREAM // no guards in dcmtk or SEM...
+#endif
+#include "DICOMTract_to_VTKCLP.h"
+
+
+#define vtkSP vtkSmartPointer
+
+// Use an anonymous namespace to keep class types and function names
+// from colliding when module is used as shared object module.  Every
+// thing should be in an anonymous namespace except for the module
+// entry point, e.g. main()
+//
+namespace
+{
+
+} // end of anonymous namespace
+
+std::vector<vtkSmartPointer< vtkPolyData> > extract_tracks(TrcTractographyResults*);
+vtkSmartPointer<vtkPolyData> trackset_to_vtk(TrcTrackSet*);
+
+//-----------------------------------------------------------------------------
+int main(int argc, char * argv[])
+{
+  PARSE_ARGS;
+  // defines:
+  //   std::string input_track_dicom
+  //   std::string output_vtk
+
+  OFCondition result;
+  TrcTractographyResults *track_dataset = NULL;
+  std::vector< vtkSmartPointer<vtkPolyData> > pdtracks;
+
+  result = TrcTractographyResults::loadFile(input_track_dicom.c_str(), track_dataset);
+  if (result.bad())
+    {
+    std::cerr << "Tractography file import failed due to error: " << result.text();
+    }
+  if (result.good())
+    {
+    pdtracks = extract_tracks(track_dataset);
+    }
+
+  static double lps_to_ras[16] = { -1, 0, 0, 0,
+                                  0,-1, 0, 0,
+                                  0, 0, 1, 0,
+                                  0, 0, 0, 1 };
+  vtkSP<vtkTransform> lps_to_ras_xfm = vtkSP<vtkTransform>::New();
+  lps_to_ras_xfm->SetMatrix(lps_to_ras);
+  vtkSP<vtkTransformPolyDataFilter> pd_xfm = vtkSP<vtkTransformPolyDataFilter>::New();
+  pd_xfm->SetInputData(pdtracks[0]);
+  pd_xfm->SetTransform(lps_to_ras_xfm);
+  pd_xfm->Update();
+  vtkSP<vtkPolyData> polydata = vtkSP<vtkPolyData>(pd_xfm->GetOutput());
+
+  // TODO
+  vtkXMLPolyDataWriter* writer = vtkXMLPolyDataWriter::New();
+  writer->SetInputData(polydata);
+  writer->SetFileName(output_vtk.c_str());
+  writer->Update();
+  writer->Delete();
+
+  return EXIT_SUCCESS;
+}
+
+size_t count_points(OFVector<TrcTrack*> tracks)
+  {
+  size_t points = 0;
+  size_t count = 0;
+  for (OFVector<TrcTrack*>::iterator iter = tracks.begin();
+       iter != tracks.end(); iter++)
+    {
+    points += (*iter)->getNumDataPoints();
+    if (count > 10)
+      exit(1);
+    }
+
+  return points;
+  }
+
+std::vector< vtkSmartPointer<vtkPolyData> >
+extract_tracks(TrcTractographyResults *trackdataset)
+  {
+  std::vector< vtkSmartPointer<vtkPolyData> > results;
+
+  OFVector<TrcTrackSet*> tracksets = trackdataset->getTrackSets();
+  if (tracksets.size() < 1)
+    {
+    std::cerr << "No tracksets in DICOM tractography results!" << std::endl;
+    return results;
+    }
+
+  DBGOUT << "Found: " << tracksets.size() << " tracksets" << std::endl;
+
+  OFVector<TrcTrackSet*>::iterator iter = tracksets.begin();
+  for (; iter != tracksets.end(); iter++)
+    {
+    vtkSmartPointer<vtkPolyData> track = trackset_to_vtk(*iter);
+    if (track.Get() == nullptr)
+      {
+      std::cerr << "no track found" << std::endl;
+      continue;
+      }
+
+    results.push_back(track);
+    }
+
+  return results;
+  }
+
+vtkSmartPointer<vtkPolyData> trackset_to_vtk(TrcTrackSet* trackset)
+  {
+  vtkSmartPointer<vtkPolyData> result_pd = vtkSmartPointer<vtkPolyData>::New();
+
+  OFVector<TrcTrack*> tracks = trackset->getTracks();
+
+  if (tracks.size() < 1) // TODO coverage
+    return result_pd; // no tracks in trackset
+
+  // pre-allocate output
+  vtkSmartPointer<vtkPoints> result_pts = vtkSmartPointer<vtkPoints>::New();
+  result_pd->SetPoints(result_pts);
+  result_pts->SetNumberOfPoints(count_points(tracks));
+
+  vtkSmartPointer<vtkCellArray> result_lines = vtkSmartPointer<vtkCellArray>::New();
+  result_pd->SetLines(result_lines);
+
+  // cell id number
+  size_t cur_cell_id = 0;
+  OFVector<TrcTrack*>::iterator track_iter = tracks.begin();
+  for (; track_iter != tracks.end(); track_iter++)
+    {
+    const float* data;
+    size_t num_pts = (*track_iter)->getTrackData(data);
+    result_lines->InsertNextCell(num_pts);
+
+    for (size_t i = 0; i < num_pts; i++)
+      {
+      result_pts->SetPoint(cur_cell_id, (data + i*3));
+      result_lines->InsertCellPoint(cur_cell_id);
+      cur_cell_id++;
+      }
+    }
+
+  return result_pd;
+  }
---
+++ Modules/Loadable/TractIO/TractIOCLI/VTK_to_DICOMTract.cxx
@@ -0,0 +1,278 @@
+// std includes
+#include <cstdint>
+#include <memory>
+#include <sstream>
+
+// VTK includes
+#include "vtkSmartPointer.h"
+#include "vtkPolyData.h"
+#include "vtkCell.h"
+#include "vtkPoints.h"
+#include "vtkFloatArray.h"
+#include "vtkDataArray.h"
+#include "vtkTransform.h"
+#include "vtkTransformPolyDataFilter.h"
+#include "vtkXMLPolyDataReader.h"
+
+// DCMTK includes
+#include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
+
+#define INCLUDE_CMATH
+#include "dcmtk/ofstd/ofstdinc.h"
+#include "dcmtk/ofstd/ofcond.h"
+#include "dcmtk/dcmiod/iodreferences.h"
+#include "dcmtk/dcmtract/trctractographyresults.h"
+#include "dcmtk/dcmtract/trctrackset.h"
+#include "dcmtk/dcmtract/trcmeasurement.h"
+#include "dcmtk/dcmtract/trctrack.h"
+
+// Slicer includes
+#ifdef HAVE_SSTREAM
+#undef HAVE_SSTREAM // no guards in dcmtk or SEM...
+#endif
+#include "VTK_to_DICOMTractCLP.h"
+
+// Shared ptr aliases
+#define SP std::shared_ptr
+#define vtkSP vtkSmartPointer
+
+#define TIO_MANUFACTURER "libTractIO";
+#define TIO_MANUFACTURER_MODELNAME "vtktodicom";
+#define TIO_DEVICESERIALNUMBER "0000";
+#define TIO_SOFTWAREVERSIONS "TractIO 0.1\\DCMTK 3.6.1";
+
+// Forward declaration
+SP<TrcTractographyResults> create_dicom(std::vector<std::string> files);
+int add_tracts(SP<TrcTractographyResults> dcmtract,
+               vtkSP<vtkPolyData> polydata,
+               std::string label = "TRACKSET");
+vtkSP<vtkPolyData> load_polydata(std::string polydata_file);
+
+int main(int argc, char *argv[])
+{
+  PARSE_ARGS;
+  // defines:
+  //   std::string vtk_fiberbundle reference_dicom output_directory output_filename
+  //   bool ras_to_lps
+
+  int result = 0;
+  std::string polydata_file = vtk_fiberbundle;
+
+  std::stringstream output_tmp;
+  output_tmp << output_directory << "/" << output_filename;
+  std::string output_file = output_tmp.str();
+
+  std::vector<std::string> ref_files = reference_dicom;
+
+  // read polydata file
+  vtkSP<vtkPolyData> polydata = load_polydata(polydata_file);
+
+  if (!polydata)
+    {
+    std::cerr << "Error: failed to load polydata" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // TODO
+  static double rastolps[16] = { -1, 0, 0, 0,
+                                  0,-1, 0, 0,
+                                  0, 0, 1, 0,
+                                  0, 0, 0, 1 };
+  vtkSP<vtkTransform> raslps_xfm = vtkSP<vtkTransform>::New();
+  raslps_xfm->SetMatrix(rastolps);
+  vtkSP<vtkTransformPolyDataFilter> pd_xfm = vtkSP<vtkTransformPolyDataFilter>::New();
+  pd_xfm->SetInputData(polydata);
+  pd_xfm->SetTransform(raslps_xfm);
+  pd_xfm->Update();
+  polydata = pd_xfm->GetOutput();
+
+  // OFLog::configure(OFLogger::TRACE_LOG_LEVEL);
+
+  // create DICOM object associated to reference files
+  SP<TrcTractographyResults> dicom = create_dicom(ref_files);
+  if (!dicom)
+    {
+    std::cerr << "Error: Tract DICOM object creation failed!" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // add tracks from polydata
+  if ( add_tracts(dicom, polydata) != 0)
+    {
+    std::cerr << "Error: Failed to add tracks from polydata." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // write DICOM to disk
+  OFCondition ofresult;
+  ofresult = dicom->saveFile(output_file.c_str());
+  if (ofresult.bad())
+    {
+    std::cerr << "Error: Failed to save tractography DICOM file." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
+/* ------------------------------------------------------------------------- */
+
+SP<TrcTractographyResults> create_dicom(std::vector<std::string> files)
+{
+  OFCondition result;
+  SP<TrcTractographyResults> tract;
+
+  ContentIdentificationMacro* contentID = NULL;
+  result = ContentIdentificationMacro::create("1", "TRACT_TEST_LABEL",
+                                              "Tractography from VTK file",
+                                              "TractIO",
+                                              contentID);
+
+  if (result.bad())
+    return tract;
+
+  // TODO: auto-convert
+  std::vector<std::string>::iterator files_iter = files.begin();
+  OFVector<OFString> of_files;
+  for (; files_iter != files.end(); files_iter++)
+    of_files.push_back( files_iter->c_str());
+
+  std::cout << "Reference file: " << of_files[0] << std::endl;
+  IODReferences references;
+  if ( references.addFromFiles(of_files) != files.size() )
+    return tract;
+
+  IODEnhGeneralEquipmentModule::EquipmentInfo eq;
+  eq.m_Manufacturer           = TIO_MANUFACTURER;
+  eq.m_ManufacturerModelName  = TIO_MANUFACTURER_MODELNAME;
+  eq.m_DeviceSerialNumber     = TIO_DEVICESERIALNUMBER;
+  eq.m_SoftwareVersions       = TIO_SOFTWAREVERSIONS;
+
+  TrcTractographyResults *p_tract = NULL;
+  result = TrcTractographyResults::create(
+             *contentID,
+             "20160329", // TODO actual date
+             "124200",   // TODO actual time
+             eq,
+             references,
+             p_tract);
+
+
+  if (result.good())
+    {
+    result = p_tract->importPatientStudyFoR(files[0].c_str(),
+                                            true, /* usePatient*/
+                                            true, /* useStudy */
+                                            true, /* useSeries */
+                                            true  /* useFoR = use Frame of Reference */
+                                            );    /*          i.e. patient space     */
+    }
+
+  if (result.good())
+    {
+    // take pointer
+    tract.reset(p_tract);
+    }
+  else
+    {
+    delete contentID;
+    contentID = NULL;
+    delete p_tract;
+    p_tract = NULL;
+    }
+
+  return tract;
+}
+
+vtkSP<vtkPolyData> load_polydata(std::string polydata_file)
+{
+  // TODO: handle traditional .vtk files
+  vtkSP<vtkXMLPolyDataReader> reader = vtkSP<vtkXMLPolyDataReader>::New();
+
+  reader->SetFileName(polydata_file.c_str());
+  reader->Update();
+
+  if (!reader->GetOutput())
+    {
+    std::cerr << "Error: failed to read VTK file \"" << polydata_file << "\"" << std::endl;
+    return vtkSP<vtkPolyData>();
+    }
+
+  return vtkSP<vtkPolyData>(reader->GetOutput());
+}
+
+int insert_polydata_tracts(TrcTrackSet *trackset,
+                           vtkSP<vtkPolyData> polydata)
+{
+  OFCondition result;
+  vtkPoints *points = polydata->GetPoints();
+  vtkSP<vtkFloatArray> pointdata = vtkSP<vtkFloatArray>::New();
+
+  // ShallowCopy will do a type-correct conversion if types do match.
+  pointdata->ShallowCopy(points->GetData());
+
+  if (pointdata->GetNumberOfTuples() < 1)
+    {
+    std::cerr << "PolyData fibertrack missing point data!" << std::endl;
+    return 1;
+    }
+
+
+  /* add points for each track */
+  for (int i = 0; i < polydata->GetNumberOfCells(); i++)
+    {
+    vtkCell *cell = polydata->GetCell(i);
+    vtkIdType numPoints   = cell->GetNumberOfPoints();
+    vtkIdType cellStartIdx = cell->GetPointId(0);
+    TrcTrack *track_dontcare = NULL;
+
+    // TODO: verify contiguity assumption is correct in general.
+    float *cellPointData = (float*)pointdata->GetVoidPointer(cellStartIdx * 3);
+    result = trackset->addTrack(cellPointData,
+                                numPoints,
+                                NULL, 0,
+                                track_dontcare);
+
+    if (result.bad())
+      {
+      std::cerr << "Error adding polydata track to trackset." << std::endl;
+      return 1;
+      }
+    }
+  return 0;
+}
+
+int add_tracts(SP<TrcTractographyResults> dcmtract,
+               vtkSP<vtkPolyData> polydata,
+               std::string label)
+{
+  assert(polydata);
+
+  OFCondition result;
+
+  CodeWithModifiers anatomyCode;
+  anatomyCode.set("T-A0095", "SRT", "White matter of brain and spinal cord");
+  CodeSequenceMacro diffusionModelCode("113231", "DCM", "Single Tensor");
+  CodeSequenceMacro algorithmCode("113211", "DCM", "Deterministic");
+
+  char buf_label[100];
+  sprintf(buf_label, "%s", label.c_str());
+
+  TrcTrackSet *trackset = NULL;
+  result = dcmtract->addTrackSet(
+    buf_label,
+    buf_label,
+    anatomyCode,
+    diffusionModelCode,
+    algorithmCode,
+    trackset);
+
+  if (result.bad())
+    return 1;
+
+  /* required by standard */
+  trackset->setRecommendedDisplayCIELabValue(1,1,1);
+
+  return insert_polydata_tracts(trackset, polydata);
+}
+
