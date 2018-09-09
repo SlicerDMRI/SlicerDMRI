bb54d2f100a1d12a23c6d0739a996ca89a3235d1
Isaiah Norton <isaiah.norton@gmail.com>
2018-09-08 23:47:50 -0400

    ENH: wrap DMRI module templates initializer in try block

--- Modules/Scripted/DMRIPlugins/RegisterDMRIModuleTemplates.py
+++ Modules/Scripted/DMRIPlugins/RegisterDMRIModuleTemplates.py
@@ -1,6 +1,6 @@
 import os
 import vtk, qt, ctk, slicer
-
+import warnings
 from slicer.ScriptedLoadableModule import *
 
 class RegisterDMRIModuleTemplates:
@@ -10,7 +10,7 @@ class RegisterDMRIModuleTemplates:
         parent.contributors = ["Isaiah Norton (Brigham & Women's Hospital)"]
         parent.helpText = ""
         parent.hidden = True
-       
+
         dmri_template_path = os.path.join(slicer.app.extensionsInstallPath,
                                           "SlicerDMRI", "DMRIModuleTemplates")
 
@@ -20,20 +20,24 @@ class RegisterDMRIModuleTemplates:
 
             if os.path.isdir(build_template_path):
                 dmri_template_path = build_template_path
-            
-            else: 
-                import warnings, textwrap
+
+            else:
+                import textwrap
                 warnings.warn(textwrap.dedent(
                     """SlicerDMRI template module path '{}' is missing.
                        Templates will not be auto-registered""").format(dmri_template_path))
                 return
 
-        import ExtensionWizardLib
-        # add the *installed* template path to the wizard lookup paths.
-        # TODO handle case of local build tree
-        settings = qt.QSettings()
-        
-        userkey = ExtensionWizardLib.TemplatePathUtilities.userTemplatePathKey()
-        modules_key = userkey + "/modules"
-        updated_value = settings.value(modules_key) + (dmri_template_path,)
-        settings.setValue(modules_key, updated_value)
+        # wrap in a try block to reduce risk of messing up startup
+        try:
+            import ExtensionWizardLib
+            # add the *installed* template path to the wizard lookup paths
+            # TODO handle case of local build tree
+            settings = qt.QSettings()
+
+            userkey = ExtensionWizardLib.TemplatePathUtilities.userTemplatePathKey()
+            modules_key = userkey + "/modules"
+            updated_value = settings.value(modules_key) + (dmri_template_path,)
+            settings.setValue(modules_key, updated_value)
+        except Exception as exc:
+            warnings.warn("Exception during attempt to register DMRI module templates: " + str(exc))
