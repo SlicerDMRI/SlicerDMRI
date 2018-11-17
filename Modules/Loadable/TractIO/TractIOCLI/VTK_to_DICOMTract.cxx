d2d64d5ec185255d1b15ba03f18c6d97b4aa998e
Isaiah Norton <ihnorton@users.noreply.github.com>
2018-11-16 21:46:55 -0500

    Dicom TR improvements (#119)
    * ENH: add Supplement 181 code mappings
    
    * WIP adding algorithm family
    
    * WIP update to use M. Onken's pending patch to trackset constructor
    
    * WIP updates, try to make Laterality work
    
    currently not saved in the correct tag in the output DICOM, emailed upstream
    
    * TractIO: support reading TR measurements into vtkPolyData scalar arrays
    
    * WIP working writer version

--- Modules/Loadable/TractIO/TractIOCLI/VTK_to_DICOMTract.cxx
+++ Modules/Loadable/TractIO/TractIOCLI/VTK_to_DICOMTract.cxx
@@ -1,29 +1,34 @@
 // std includes
 #include <memory>
 #include <sstream>
+using std::string;
 
 // VTK includes
-#include "vtkSmartPointer.h"
-#include "vtkPolyData.h"
-#include "vtkCell.h"
-#include "vtkPoints.h"
-#include "vtkFloatArray.h"
-#include "vtkDataArray.h"
-#include "vtkTransform.h"
-#include "vtkTransformPolyDataFilter.h"
-#include "vtkXMLPolyDataReader.h"
+#include <vtkPolyData.h>
+#include <vtkCell.h>
+#include <vtkCellData.h>
+#include <vtkPoints.h>
+#include <vtkPointData.h>
+#include <vtkFloatArray.h>
+#include <vtkDataArray.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkXMLPolyDataReader.h>
 
 // DCMTK includes
 #include "dcmtk/config/osconfig.h"    /* make sure OS specific configuration is included first */
 
 #define INCLUDE_CMATH
+#include "dcmtk/dcmsr/codes/dcm.h"
 #include "dcmtk/ofstd/ofstdinc.h"
 #include "dcmtk/ofstd/ofcond.h"
 #include "dcmtk/dcmiod/iodreferences.h"
+#include "dcmtk/dcmiod/iodcontentitemmacro.h"
 #include "dcmtk/dcmtract/trctractographyresults.h"
 #include "dcmtk/dcmtract/trctrackset.h"
 #include "dcmtk/dcmtract/trcmeasurement.h"
 #include "dcmtk/dcmtract/trctrack.h"
+#include "dcmtk/dcmtract/trctypes.h"
 
 // Slicer includes
 #ifdef HAVE_SSTREAM
@@ -43,11 +48,58 @@
 // Derived from Isaiah's Medical Connections UID root
 #define SLICERDMRI_UID_SERIES_ROOT "1.2.826.0.1.3680043.9.7239.2.1"
 
+// Mapping from text keys to Supplement 181 family codes in DCMTK dictionary
+std::map<string, DSRBasicCodedEntry> algorithmFamily_keys = {
+  { "Deterministic", CODE_DCM_DeterministicTrackingAlgorithm },
+  { "Probabilistic", CODE_DCM_ProbabilisticTrackingAlgorithm },
+  { "Global", CODE_DCM_GlobalTrackingAlgorithm },
+  { "FACT", CODE_DCM_FACT },
+  { "Streamline", CODE_DCM_Streamline },
+  { "TEND", CODE_DCM_TEND },
+  { "Bootstrap", CODE_DCM_BootstrapTrackingAlgorithm },
+  { "Euler", CODE_DCM_Euler },
+  { "RungeKutta", CODE_DCM_RungeKutta }
+};
+
+std::map<string, DSRBasicCodedEntry> diffusionValue_keys = {
+  { "Trace", CODE_DCM_Trace },
+  { "MeanDiffusivity", CODE_DCM_MeanDiffusivity },
+  { "RadialDiffusivity", CODE_DCM_RadialDiffusivity },
+  { "AxialDiffusivity", CODE_DCM_AxialDiffusivity },
+  { "MeanKurtosis", CODE_DCM_MeanKurtosis },
+  { "ApparentKurtosisCoefficient", CODE_DCM_ApparentKurtosisCoefficient },
+  { "RadialKurtosis", CODE_DCM_RadialKurtosis },
+  { "AxialKurtosis", CODE_DCM_AxialKurtosis },
+  { "FractionalKurtosisAnisotropy", CODE_DCM_FractionalKurtosisAnisotropy },
+  { "VolumetricDiffusionDxxComponent", CODE_DCM_VolumetricDiffusionDxxComponent },
+  { "VolumetricDiffusionDxyComponent", CODE_DCM_VolumetricDiffusionDxyComponent },
+  { "VolumetricDiffusionDxzComponent", CODE_DCM_VolumetricDiffusionDxzComponent },
+  { "VolumetricDiffusionDyyComponent", CODE_DCM_VolumetricDiffusionDyyComponent },
+  { "VolumetricDiffusionDyzComponent", CODE_DCM_VolumetricDiffusionDyzComponent },
+  { "VolumetricDiffusionDzzComponent", CODE_DCM_VolumetricDiffusionDzzComponent }
+};
+
+std::map<string, TrcTypes::E_TrackSetLaterality> laterality_keys = {
+  {"Unknown", TrcTypes::LAT_UNKNOWN },
+  {"Left", TrcTypes::LAT_LEFT },
+  {"Right", TrcTypes::LAT_RIGHT },
+  {"RightAndLeft", TrcTypes::LAT_RIGHT_AND_LEFT },
+  {"Unilateral", TrcTypes::LAT_UNILATERAL }
+};
+
+typedef struct {
+  std::string label;
+  DSRBasicCodedEntry AlgorithmFamily;
+  std::string AlgorithmVersion;
+  std::string AlgorithmName;
+  std::string Laterality;
+} TrackInfo;
+
 // Forward declaration
 SP<TrcTractographyResults> create_dicom(std::vector<std::string> files);
 int add_tracts(SP<TrcTractographyResults> dcmtract,
                vtkSP<vtkPolyData> polydata,
-               std::string label = "TRACKSET");
+               TrackInfo info);
 vtkSP<vtkPolyData> load_polydata(std::string polydata_file);
 
 int main(int argc, char *argv[])
@@ -65,6 +117,29 @@ int main(int argc, char *argv[])
   std::string output_file = output_tmp.str();
 
   std::vector<std::string> ref_files = reference_dicom;
+
+  // validate the algorithmFamily and set corresponding code
+  auto findAlgoFamily = algorithmFamily_keys.find(AlgorithmFamily);
+  if (findAlgoFamily == algorithmFamily_keys.end())
+    {
+    std::cerr << "Invalid or missing 'algorithmFamily', please see value enumeration in --help." << std::endl;
+    return EXIT_FAILURE;
+    }
+  DSRBasicCodedEntry AlgorithmFamilyCode = findAlgoFamily->second;
+
+  if (AlgorithmVersion.empty())
+    {
+    std::cerr << "Missing required 'AlgorithmVersion' argument";
+    return EXIT_FAILURE;
+    }
+
+  TrackInfo info{
+    "TRACKSET" /*TODO*/,
+    AlgorithmFamilyCode,
+    AlgorithmVersion,
+    AlgorithmName,
+    Laterality
+  };
 
   // read polydata file
   vtkSP<vtkPolyData> polydata = load_polydata(polydata_file);
@@ -88,7 +163,7 @@ int main(int argc, char *argv[])
   pd_xfm->Update();
   polydata = pd_xfm->GetOutput();
 
-  // OFLog::configure(OFLogger::TRACE_LOG_LEVEL);
+  OFLog::configure(OFLogger::TRACE_LOG_LEVEL);
 
   // create DICOM object associated to reference files
   SP<TrcTractographyResults> dicom = create_dicom(ref_files);
@@ -104,7 +179,7 @@ int main(int argc, char *argv[])
   series_mod.setSeriesInstanceUID(dcmGenerateUniqueIdentifier(uid, SLICERDMRI_UID_SERIES_ROOT));
 
   // add tracks from polydata
-  if ( add_tracts(dicom, polydata) != 0)
+  if ( add_tracts(dicom, polydata, info) != 0)
     {
     std::cerr << "Error: Failed to add tracks from polydata." << std::endl;
     return EXIT_FAILURE;
@@ -134,7 +209,7 @@ SP<TrcTractographyResults> create_dicom(std::vector<std::string> files)
   ContentIdentificationMacro* contentID = NULL;
   result = ContentIdentificationMacro::create("1", "TRACT_TEST_LABEL",
                                               "Tractography from VTK file",
-                                              "TractIO",
+                                              "TractIO^Library",
                                               contentID);
 
   if (result.bad())
@@ -215,6 +290,13 @@ int insert_polydata_tracts(TrcTrackSet *trackset,
 {
   OFCondition result;
   vtkPoints *points = polydata->GetPoints();
+
+  if (!points)
+    {
+    std::cerr << "PolyData fibertrack missing points!" << std::endl;
+    return 1;
+    }
+
   vtkSP<vtkFloatArray> pointdata = vtkSP<vtkFloatArray>::New();
 
   // ShallowCopy will do a type-correct conversion if types do match.
@@ -228,7 +310,7 @@ int insert_polydata_tracts(TrcTrackSet *trackset,
 
 
   /* add points for each track */
-  for (int i = 0; i < polydata->GetNumberOfCells(); i++)
+  for (size_t i = 0; i < polydata->GetNumberOfCells(); i++)
     {
     vtkCell *cell = polydata->GetCell(i);
     vtkIdType numPoints   = cell->GetNumberOfPoints();
@@ -248,24 +330,90 @@ int insert_polydata_tracts(TrcTrackSet *trackset,
       return 1;
       }
     }
+
   return 0;
 }
 
+
+int insert_polydata_scalars(TrcTrackSet* trackset,
+                            vtkSP<vtkPolyData> polydata)
+  {
+  /* add measurements (scalars) for each track, if any */
+  vtkSP<vtkCellData> cells(polydata->GetCellData());
+  vtkSP<vtkPointData> pointdata = vtkSP<vtkPointData>(polydata->GetPointData());
+  std::vector<float> tmp_float;
+
+  size_t numarrays = pointdata->GetNumberOfArrays();
+  if (numarrays < 1)
+    return -1;
+
+  for (size_t scalar_idx = 0;
+       scalar_idx < numarrays;
+       scalar_idx++)
+    {
+    vtkCellArray* lines = polydata->GetLines();
+    vtkDataArray* array = pointdata->GetArray(scalar_idx);
+    size_t numtracks = polydata->GetNumberOfLines();
+
+    if ((lines == nullptr) || (numtracks < 1))
+      continue;
+
+    // TODO make this generic (at least up to standard measures + "custom")
+    CodeSequenceMacro typeCode("110808", "DCM", "Fractional Anisotropy");
+    CodeSequenceMacro unitCode("1", "UCUM", "no units");
+
+    TrcMeasurement* measurement;
+    //OFCondition res = TrcMeasurement::create(typeCode, unitCode, measurement);
+    OFCondition res = trackset->addMeasurement(typeCode, unitCode, measurement);
+
+    vtkIdType* ptids;
+    vtkIdType numpts;
+    lines->InitTraversal();
+    size_t track_idx = 0;
+
+    while (lines->GetNextCell(numpts, ptids))
+      {
+      //= ptids->GetNumberOfIds();
+      tmp_float.reserve(numpts);
+
+      // copy the measures to tmp array. (no contiguity assumption for cells)
+      for (size_t pt_idx = 0; pt_idx < numpts; pt_idx++)
+        tmp_float.push_back(*array->GetTuple(pt_idx));
+
+      // copy the data into the TrcMeasurement
+      measurement->setTrackValues(track_idx, tmp_float.data(), numpts);
+
+      track_idx++;
+      }
+
+    }
+
+  return 0;
+  }
+
+
 int add_tracts(SP<TrcTractographyResults> dcmtract,
                vtkSP<vtkPolyData> polydata,
-               std::string label)
+               TrackInfo info)
 {
   assert(polydata);
 
   OFCondition result;
 
   CodeWithModifiers anatomyCode("3");
-  anatomyCode.set("T-A0095", "SRT", "White matter of brain and spinal cord");
+    anatomyCode.set("T-A0095", "SRT", "White matter of brain and spinal cord");
+
   CodeSequenceMacro diffusionModelCode("113231", "DCM", "Single Tensor");
-  CodeSequenceMacro algorithmCode("113211", "DCM", "Deterministic");
+
+  AlgorithmIdentificationMacro algorithmId;
+  algorithmId.getAlgorithmFamilyCode().set("113211", "DCM", "Deterministic");
+  algorithmId.setAlgorithmName(info.AlgorithmName.c_str());
+  algorithmId.setAlgorithmVersion(info.AlgorithmVersion.c_str());
+  algorithmId.setAlgorithmParameters("");
+  algorithmId.setAlgorithmSource("");
 
   char buf_label[100];
-  sprintf(buf_label, "%s", label.c_str());
+  sprintf(buf_label, "%s", info.label.c_str());
 
   TrcTrackSet *trackset = NULL;
   result = dcmtract->addTrackSet(
@@ -273,8 +421,10 @@ int add_tracts(SP<TrcTractographyResults> dcmtract,
     buf_label,
     anatomyCode,
     diffusionModelCode,
-    algorithmCode,
+    algorithmId,
     trackset);
+
+  trackset->setLaterality(laterality_keys[info.Laterality], false);
 
   if (result.bad())
     return 1;
@@ -282,6 +432,9 @@ int add_tracts(SP<TrcTractographyResults> dcmtract,
   /* required by standard */
   trackset->setRecommendedDisplayCIELabValue(1,1,1);
 
-  return insert_polydata_tracts(trackset, polydata);
+  // TODO check error
+  insert_polydata_tracts(trackset, polydata);
+
+  return insert_polydata_scalars(trackset, polydata);
 }
 
