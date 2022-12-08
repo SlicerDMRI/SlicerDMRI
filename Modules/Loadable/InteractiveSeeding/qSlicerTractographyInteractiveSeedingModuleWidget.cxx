SlicerDMRI/SlicerDMRI

commit c2e8f73b1c6d4f9dd8d513362a5d00d2cdc0201f

ENH, IntxSeeding: use enums for display mode too

Modules/Loadable/InteractiveSeeding/qSlicerTractographyInteractiveSeedingModuleWidget.cxx
@@ -247,8 +247,8 @@ void qSlicerTractographyInteractiveSeedingModuleWidget::setup()
                 SLOT(setFiducialRegionStep(double)));
 
   QObject::connect(d->DisplayTracksComboBox,
-                SIGNAL(currentIndexChanged(int)),
-                SLOT(setTrackDisplayMode(int)));
+                SIGNAL(currentIndexChanged(const QString&)),
+                SLOT(setTrackDisplayMode(const QString&)));
 
   QObject::connect(d->SeedSelectedCheckBox,
                 SIGNAL(stateChanged(int)),
@@ -614,12 +614,22 @@ void qSlicerTractographyInteractiveSeedingModuleWidget::updateWidgetFromMRML()
     d->StoppingCurvatureSpinBox->setValue(paramNode->GetStoppingCurvature());
     d->StoppingCriteriaComboBox->setCurrentIndex(paramNode->GetThresholdMode());
     d->StoppingValueSpinBox->setValue(paramNode->GetStoppingValue());
-    d->DisplayTracksComboBox->setCurrentIndex(paramNode->GetDisplayMode());
     d->ROILabelInput->setText(paramNode->ROILabelsToString().c_str());
     d->RandomGridCheckBox->setChecked(paramNode->GetRandomGrid());
     d->UseIndexSpaceCheckBox->setChecked(paramNode->GetUseIndexSpace());
     d->StartThresholdSlider->setValue(paramNode->GetStartThreshold());
     d->SeedSpacingSlider->setValue(paramNode->GetSeedSpacing());
+
+    { // Use enums for display mode
+      QString target;
+      switch (paramNode->GetDisplayMode())
+        {
+        case vtkMRMLTractographyInteractiveSeedingNode::Tubes: target = "Tubes";
+        case vtkMRMLTractographyInteractiveSeedingNode::Lines: target = "Lines";
+        default: assert("Unknown display mode type!"); target = "Lines";
+        }
+      d->DisplayTracksComboBox->setCurrentIndex(d->DisplayTracksComboBox->findText(target));
+    }
 
     d->ParameterNodeSelector->setCurrentNode(
       this->mrmlScene()->GetNodeByID(paramNode->GetID()));
@@ -713,12 +723,17 @@ void qSlicerTractographyInteractiveSeedingModuleWidget::setStoppingCriteria(cons
 }
 
 //-----------------------------------------------------------------------------
-void qSlicerTractographyInteractiveSeedingModuleWidget::setTrackDisplayMode(int value)
+void qSlicerTractographyInteractiveSeedingModuleWidget::setTrackDisplayMode(const QString& value)
 {
-  if (this->TractographyInteractiveSeedingNode)
-    {
-    this->TractographyInteractiveSeedingNode->SetDisplayMode(value);
-    }
+  if (NULL == this->TractographyInteractiveSeedingNode)
+    return;
+
+  if (value == "Lines")
+    this->TractographyInteractiveSeedingNode->SetDisplayMode(vtkMRMLTractographyInteractiveSeedingNode::Lines);
+  else if (value == "Tubes")
+    this->TractographyInteractiveSeedingNode->SetDisplayMode(vtkMRMLTractographyInteractiveSeedingNode::Tubes);
+  else
+    assert("Unhandled Track Display Mode");
 }
 
 //-----------------------------------------------------------------------------

Modules/Loadable/InteractiveSeeding/Logic/vtkMRMLTractographyInteractiveSeedingNode.h
@@ -117,6 +117,7 @@ public:
   // Get/Set Display Mode (module parameter)
   // 0 - Lines
   // 1 - Tubes
+  enum { Lines = 0, Tubes = 1 };
   vtkGetMacro(DisplayMode, int);
   vtkSetMacro(DisplayMode, int);
 

Modules/Loadable/InteractiveSeeding/qSlicerTractographyInteractiveSeedingModuleWidget.h
@@ -82,7 +82,7 @@ public slots:
   void setStoppingCriteria(const QString&);
 
   /// Set display mode 0-line 1-tube
-  void setTrackDisplayMode(int value);
+  void setTrackDisplayMode(const QString&);
 
   /// Set stopping curvature
   void setStoppingCurvature(double value);

commit e293eae8da414698727d8bef567b48a5df4b090b

BUG: Fix update of DisplayTracksComboBox from MRML
This commit fixes a regression originally introduced
in c2e8f73b1 (ENH, IntxSeeding: use enums for display mode too) and
identified while addressing -Wimplicit-fallthrough warning.

Modules/Loadable/InteractiveSeeding/qSlicerTractographyInteractiveSeedingModuleWidget.cxx
@@ -624,8 +624,8 @@ void qSlicerTractographyInteractiveSeedingModuleWidget::updateWidgetFromMRML()
       QString target;
       switch (paramNode->GetDisplayMode())
         {
-        case vtkMRMLTractographyInteractiveSeedingNode::Tubes: target = "Tubes";
-        case vtkMRMLTractographyInteractiveSeedingNode::Lines: target = "Lines";
+        case vtkMRMLTractographyInteractiveSeedingNode::Tubes: target = "Tubes"; break;
+        case vtkMRMLTractographyInteractiveSeedingNode::Lines: target = "Lines"; break;
         default: assert("Unknown display mode type!"); target = "Lines";
         }
       d->DisplayTracksComboBox->setCurrentIndex(d->DisplayTracksComboBox->findText(target));